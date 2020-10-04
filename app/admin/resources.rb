c512d06b78d08505a819794135810dceb31a3a65
= <=>
2020-10-03 22:16:26 -0700

    Implemented pg_search (final commit)

--- app/admin/resources.rb
+++ app/admin/resources.rb
@@ -1,18 +1,8 @@
 ActiveAdmin.register Resource do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :title, :description, :amount, :source, :cp
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:title, :description, :amount, :source, :cp]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-
+    remove_filter :title
+    remove_filter :cp
+    remove_filter :amount
+    remove_filter :source
+    remove_filter :description
 end
