3d1cbc5fbc16ab60633859e1d41189e72fd0b551
Ubuntu <ubuntu@ip-172-31-4-85.us-east-2.compute.internal>
2020-11-28 02:34:22 +0000

    gutentag prelimiaries

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,18 @@
 class SearchController < ApplicationController
   def index
    if params[:query].present?
-     @resources = Resource.search(params[:query])
+        if params[:notFinAid].present?
+            @resources = Resource.search(params[:query]).tagged_with(:ids => [:notFinAid], :match => :any)
+        else
+            @resources = Resource.search(params[:query])
+        end
+     # Article.tagged_with(:ids => [tag_a.id, tag_b.id], :match => :all)
    else
-     @resources = Resource.all
+        if params[:notFinAid].present?
+            @resources = Resource.search(params[:query]).tagged_with(:ids => [:notFinAid], :match => :any)
+        else
+            @resources = Resource.all
+        end
    end
   end
 end
