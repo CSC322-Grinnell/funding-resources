ee3a993690adb714a8b158f02c787eeb7345761a
Rob Lorch <lorchrob@gmail.com>
2020-11-30 21:23:35 +0000

    Update search controller to filter by notFinAid

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,14 +2,13 @@ class SearchController < ApplicationController
   def index
    if params[:query].present?
         if params[:notFinAid].present?
-            @resources = Resource.search(params[:query]).tagged_with(:ids => [:notFinAid], :match => :any)
+            @resources = Resource.tagged_with(:names => ['notFinAid'], :match => :any).search(params[:query])
         else
             @resources = Resource.search(params[:query])
         end
-     # Article.tagged_with(:ids => [tag_a.id, tag_b.id], :match => :all)
    else
         if params[:notFinAid].present?
-            @resources = Resource.search(params[:query]).tagged_with(:ids => [:notFinAid], :match => :any)
+            @resources = Resource.tagged_with(:names => ['notFinAid'], :match => :any)
         else
             @resources = Resource.all
         end
