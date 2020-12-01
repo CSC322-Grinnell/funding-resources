53e795d110f5c56c2d7520f8e59ae1516297f723
Rob Lorch <lorchrob@gmail.com>
2020-12-01 17:44:24 +0000

    Generalize tags in navbar and search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,17 +1,28 @@
 class SearchController < ApplicationController
   def index
-   if params[:query].present?
-        if params[:notFinAid].present?
-            @resources = Resource.tagged_with(:names => ['notFinAid'], :match => :any).search(params[:query])
-        else
-            @resources = Resource.search(params[:query])
+    @tags = Gutentag::Tag.all
+    @tag_names_selected = []
+    
+    # Get an array of all the tags checked
+    params.each do |k, v|
+        if k != "query" and k != "commit" and k != "controller" and k != "action"
+            @tag_names_selected.push(k)
         end
-   else
-        if params[:notFinAid].present?
-            @resources = Resource.tagged_with(:names => ['notFinAid'], :match => :any)
-        else
-            @resources = Resource.all
-        end
-   end
+    end
+    
+    if params[:query].present? # If there is a query
+      if @tag_names_selected.length() > 0 # If there are tags
+        @resources = Resource.tagged_with(:names => @tag_names_selected, :match => :any).search(params[:query])
+      else # No tags
+        @resources = Resource.search(params[:query])
+      end
+      
+    else # If the search bar is blank
+      if @tag_names_selected.length() > 0 # If there are tags
+        @resources = Resource.tagged_with(:names => @tag_names_selected, :match => :any)
+      else # No tags
+        @resources = Resource.all
+      end
+    end
   end
 end
